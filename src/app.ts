d4419f7a137b97167a1204cdc5ca3563bcd38617
SanariSan <SanariSan@users.noreply.github.com>
2021-09-22 16:07:33 +0300

    !feat testing blessed(-contrib) modules
    decided to use emitter as a way ow distributing info to screen panels
    since using @xtcry/blessed, blessed-contrib package couldn't find "blessed", so had to yarn link it (WITH CHANGING PACKAGE NAME, AFTER UPDATE - REDO)

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,11 @@ import { sendForm, sendLinkParams, sendQueryParams } from "./api-wrappers";
 import { CliCheckbox, CliConfirm, CliEmail, CliList, CliNum, CliPass, CliText } from "./core/cli";
 import { exampleCLI } from "./core/cli/examples/example";
 import { handleError } from "./core/errors";
+import blessed from "@xtcry/blessed";
+import contrib from "blessed-contrib";
+import { EventEmitter } from "stream";
+
+// const { EventEmitter } = require("events");
 
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
@@ -30,21 +35,105 @@ async function main() {
 	return true;
 }
 
+async function test() {
+	var screen = blessed.screen();
+	const firstEmitter = new EventEmitter();
+
+	const listeners = {
+		logsListenersMap: new Map(),
+		otherListenersMap: new Map(),
+	};
+
+	//this wrapper lets us access moments right before<->after cb fn call
+	const handlerWrap = (fn) => {
+		const _handler = (...args) => {
+			fn(...args);
+		};
+
+		return _handler;
+	};
+
+	const setListener = (
+		emitter: EventEmitter,
+		eventName,
+		listenersMap,
+		handler, //can remove or save listener (handler) from here
+	) => {
+		emitter.on(eventName, handler);
+		listenersMap.set(eventName, handler); //will do from here
+	};
+
+	const clearListeners = (emitter: EventEmitter, listenersMap) => {
+		for (let [listenerName, handler] of listenersMap) {
+			emitter.off(listenerName, handler);
+		}
+	};
+
+	function page1(screen) {
+		clearListeners(firstEmitter, listeners.logsListenersMap);
+
+		var grid = new contrib.grid({ rows: 12, cols: 12, screen: screen });
+		var logScr = grid.set(0, 0, 10, 10, contrib.log, {
+			label: "Logs",
+			bufferLength: 1000,
+		});
+		var logSwitch = grid.set(0, 10, 10, 2, contrib.log, { label: "Switch" });
+		var notifications = grid.set(10, 0, 2, 12, blessed.box, { content: "Notifications" });
+
+		setListener(
+			firstEmitter,
+			"log",
+			listeners.logsListenersMap,
+			handlerWrap((str) => logScr.log(str)),
+		);
+
+		screen.render();
+	}
+
+	function page2(screen) {
+		var grid = new contrib.grid({ rows: 12, cols: 12, screen: screen });
+		//grid.set(row, col, rowSpan, colSpan, obj, opts)
+		var questions = grid.set(0, 0, 12, 6, contrib.log, { label: "Questions" });
+		var stats = grid.set(0, 6, 12, 6, blessed.box, { content: "Stats" });
+		screen.render();
+	}
+
+	//no type for carousel ?....
+	// @ts-ignore
+	var carousel = new contrib.carousel([page1, page2], {
+		screen: screen,
+		controlKeys: true,
+	});
+
+	let i = 0;
+	setInterval(function () {
+		firstEmitter.emit("log", `${i++} - ${firstEmitter.listenerCount("log")}`);
+	}, 150);
+
+	carousel.start();
+
+	screen.key(["escape", "q", "C-c"], function (ch, key) {
+		return process.exit(0);
+	});
+}
+
 async function init() {
 	let error = false;
 
-	do {
-		const cliConfirm = new CliConfirm();
-		await cliConfirm.prompt({ key: "key", question: "Ready for the iteration?" });
+	test();
 
-		// await exampleCLI();
+	// do {
+	// 	const cliConfirm = new CliConfirm();
+	// 	await cliConfirm.prompt({ key: "key", question: "Ready for the iteration?" });
 
-		error = false;
-		await main().catch((e) => {
-			error = true;
-			handleError(e);
-		});
-	} while (error);
+	// 	// await exampleCLI();
+
+	// 	error = false;
+	// 	await main().catch((e) => {
+	// 		error = true;
+	// 		handleError(e);
+	// 	});
+	// } while (error);
 }
 
 init();
